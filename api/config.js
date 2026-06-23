0b78abad8d3ed928c73e155828a23244f54eaa44
coder-shivam <innocentboy9472@gmail.com>
2026-06-23 13:51:29 +0530

    Update config.js

--- api/config.js
+++ api/config.js
@@ -7,13 +7,15 @@ module.exports = function handler(req, res) {
   const adScriptUrl = process.env.AD_SCRIPT_URL || process.env.NEXT_PUBLIC_AD_SCRIPT_URL || '';
   const adZoneId = process.env.AD_ZONE_ID || process.env.NEXT_PUBLIC_AD_ZONE_ID || '';
   const vignetteId = process.env.VIGNETTE_ID || process.env.NEXT_PUBLIC_VIGNETTE_ID || '';
-  const pushId = process.env.PUSH_ID || process.env.NEXT_PUBLIC_PUSH_ID || '';
+  const inPagePushId = process.env.IN_PAGE_PUSH_ID || process.env.NEXT_PUBLIC_IN_PAGE_PUSH_ID || process.env.PUSH_ID || process.env.NEXT_PUBLIC_PUSH_ID || '11189726';
+  const pushNotifyId = process.env.PUSH_NOTIFY_ID || process.env.NEXT_PUBLIC_PUSH_NOTIFY_ID || '11189728';
 
   return res.status(200).json({
     redirectUrl,
     adScriptUrl,
     adZoneId,
     vignetteId,
-    pushId
+    inPagePushId,
+    pushNotifyId
   });
 };
